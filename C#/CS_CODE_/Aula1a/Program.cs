f7a6e6c6e86668e84cede09eda2e8eef028e17b6
gui-sa <guime.sa9@gmail.com>
2022-11-21 22:02:55 -0300

    Aula1 CSuisse

---
+++ C#/CS_CODE_/Aula1a/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula1a{
+    class Aula1a{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+            //Variaveis
+            int valorInteiro=10;
+            System.Console.WriteLine(valorInteiro);
+            valorInteiro =200;
+            Console.WriteLine("Alterando o int:");
+            System.Console.WriteLine(valorInteiro);
+
+            float valorFloat = 10.4F;
+            double valorDouble = 11.78D;
+            double valorDouble2 = 11.89;
+            
+            decimal valorDecimal = 15.84M; 
+
+            char valorChar = 'A';
+            string  valorString = "Frase gigante";
+
+            bool valorBooleano = true;
+            
+
+
+        }
+    }
+}
+ 
