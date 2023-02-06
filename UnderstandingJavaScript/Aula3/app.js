4b6d1c73d0213494c7e0123a3bc241b9e2f24ea2
gui-sa <guime.sa9@gmail.com>
2023-02-06 16:56:18 -0300

    06 fev study

---
+++ UnderstandingJavaScript/Aula3/app.js
@@ -0,0 +1,7 @@
+//Coercion
+//var a = 1 + '2';
+//console.log(a);
+//Returns 12 due 1 coercion
+
+var a = 1, b =  '2';
+console.log(a+b);
---
+++ UnderstandingJavaScript/Aula4/app.js
@@ -0,0 +1,7 @@
+//Case 1
+//console.log(3<2<1); // will return a true , instead of false because
+//Number(false) is 0 e Number(true) is 1 -- coercion
+//SO, 3<2 is false = 0. 0<1 is true.
+
+//If you cant coerce ir, it will return as NaN
+ 
