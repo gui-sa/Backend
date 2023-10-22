d84dcb8f724a6bf240777b37ef672c12e05a1585
gui-sa <guime.sa9@gmail.com>
2023-10-22 04:28:36 -0300

    Finaliza a atividade arrumando os bugs e as ideias 2

--- WebPack/LazyLoad/Images/style.js
+++ WebPack/LazyLoad/Images/style.js
@@ -1,10 +1,13 @@
 const blurDivs = document.querySelectorAll(".blur-load");
 
 blurDivs.forEach(div=>{
+    div.style.setProperty('--animation-set',"pulse 2.5s infinite");
     const img = div.querySelector("img");
+    img.style.opacity = 0;
 
     function loaded(){
         div.classList.add("loaded");
+        img.style.opacity = 1;
     }
 
     if(img.complete){
